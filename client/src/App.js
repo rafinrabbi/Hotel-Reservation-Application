068084cbb6d2eee70ac5b399ea7f21fbe57e5332
Rawhatur Rabbi Rafin <rawhaturrafin@gmail.com>
2022-07-16 16:59:03 +0600

    resolve client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Coming Soon</h1>
+      <h2>
+Hotel-Reservation-Application</h2>
+    </div>
+  );
+}
+
+export default App;
